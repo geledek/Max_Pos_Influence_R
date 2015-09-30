edd222357ab7a47c8a6ac9736db31f65134be3c6
DaBingshui <blackstardbs@gmail.com>
2015-09-30 15:14:12 +0800

    weight added

--- graph_gen.R
+++ graph_gen.R
@@ -1,10 +1,13 @@
 library("igraph")
 
-el=read.csv(file.choose())
+el=read.csv(file.choose(), sep=" ")
 el[,1]=as.character(el[,1]) 
 el[,2]=as.character(el[,2])
 el=as.matrix(el) 
-g=graph.edgelist(el[1:10,1:2]) # load the first 10 lines
-E(g)$weight=as.numeric(el[,3])
+
+g=graph.edgelist(el[1:100,1:2]) # load the first 10 lines
+
+E(g)$weight=as.numeric(runif(length(g),min = 0, max = 1))
+E(g)$weight /strength(g,mode="out")[get.edgelist(g)[,1]]
 
 plot(g,layout=layout.fruchterman.reingold,edge.width=E(g)$weight/2)
