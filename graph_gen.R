f85bcd63dbc350cef077429850d0d8a2c7c8af9e
blackstardbs@gmail.com <blackstardbs@gmail.com>
2015-10-01 15:13:49 +0800

    Node weight

--- graph_gen.R
+++ graph_gen.R
@@ -5,9 +5,11 @@ el[,1]=as.character(el[,1])
 el[,2]=as.character(el[,2])
 el=as.matrix(el) 
 
-g=graph.edgelist(el[1:100,1:2]) # load the first 10 lines
+g=graph.edgelist(el[,1:2]) # load the first 10 lines
 
 E(g)$weight=as.numeric(runif(length(g),min = 0, max = 1))
 E(g)$weight /strength(g,mode="out")[get.edgelist(g)[,1]]
+V(g)$weight=as.numeric(runif(length(V(g)),min = 0, max = 1))
+
 
 plot(g,layout=layout.fruchterman.reingold,edge.width=E(g)$weight/2)
