ecc7c2e40493a6704d5e4d0f3ab51499420ce2c6
geledek <geledek@gmail.com>
2015-10-03 22:16:50 +0800

    algorithm 2 added

--- main_script.R
+++ main_script.R
@@ -1,10 +1,13 @@
 source("functions.R")
 source("greedy.R")
 source("greedy_plus.R")
+source("ovm_select.R")
 
-g <- InitiateGraph()
+g <- InitiateGraph(50) # pass number of edges as argument
 PlotPreConfig(g)
 message("Greedy")
 Greedy(g, 10)
 message("Greedy_plus")
 GreedyPlus(g,10)
+message("OVM-Potential Candidate Selection")
+OVMSelect(g,10,4)
